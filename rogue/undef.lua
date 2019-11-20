e14c0b99e6a7b5ad8660163596a51d39b012568f
gengbing <edwingeng@126.com>
2019-11-21 00:31:29 +0800

    improve undef

--- rogue/undef.lua
+++ rogue/undef.lua
@@ -1,9 +1,10 @@
+local undefined = function()
+    error("undefined")
+end
+
 undef = setmetatable({}, {
-    __index = function()
-        error("undefined")
-    end,
-    __newindex = function()
-        error("undefined")
-    end,
+    __len = undefined,
+    __index = undefined,
+    __newindex = undefined,
     __metatable = false
 })

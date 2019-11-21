edd528d8e672324b58818b03aad51c38afd18353
gengbing <edwingeng@126.com>
2019-11-22 00:42:09 +0800

    merge calc.go and fibs.go into hello.go

--- example/hello.go
+++ example/hello.go
@@ -4,3 +4,20 @@ func init() {
 	print("Hello ")
 	println("World!")
 }
+
+func Add(n1, n2 int) int {
+	return n1 + n2
+}
+
+func Sub(n1 int, n2 int) int {
+	n3 := n1 - n2
+	return n3
+}
+
+func Fibs(n int) int {
+	if n == 1 || n == 2 {
+		return 1
+	}
+
+	return Fibs(n-1) + Fibs(n-2)
+}
--- example/hello.lua
+++ example/hello.lua
@@ -5,4 +5,21 @@ local init = function()
     print("World!")
 end
 
+Add = function(n1, n2)
+    return n1 + n2
+end
+
+Sub = function(n1, n2)
+    local n3 = n1 - n2
+    return n3
+end
+
+Fibs = function(n)
+    if n == 1 or n == 2 then
+        return 1
+    end
+
+    return Fibs(n - 1) + Fibs(n - 2)
+end
+
 return init
